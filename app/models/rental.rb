a2c8a32c3207667c31e1efa519bb4752cc77f5d7
Lewis Silletto <lsilletto@caredash.com>
2021-05-16 22:04:46 -0400

    Create slug on save

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,12 @@
 class Rental < ApplicationRecord
+  before_create :slugify
+
   # For simplicity of the Ember App, require everything.
   [:title, :owner, :city, :category, :bedrooms, :image, :description, :lat, :lng].each do |field|
     validates field, presence: true
+  end
+
+  def slugify
+    self.slug = self.title.parameterize
   end
 end
